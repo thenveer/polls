0fd2660d5b981c58c506a93264951c84a87a7118
muhamed <thenveer81@gmail.com>
2020-03-29 14:29:57 +0530

    updated STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-4cnqu-%-a2#s#7%20m1ya)tmzv$bv5u2(-j^(6d&gq$1o4dtn'
-
+#SECRET_KEY = '-4cnqu-%-a2#s#7%20m1ya)tmzv$bv5u2(-j^(6d&gq$1o4dtn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mypollsapp.herokuapp.com']
 
 
 # Application definition
